ca61b82c95f8cbdff3f1eeb4fcce8a78f126044c
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-03 01:03:43 +0600

    adding chefs json file and creacting id wise json

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const chefs = require('./chefs.json');
 
+app.use(cors());
 
 app.get("/", (req,res) => {
     res.send('welcome to Chefs Table');
+})
+
+app.get("/chefs",(req,res) => {
+    res.send(chefs);
+    // console.log(req);
+})
+
+app.get("/chefs/:id", (req,res) => {
+    const id = Number(req.params.id);
+console.log(id);
+    const newData = chefs.find(c => c.id === id);
+
+    res.send(newData);
 })
 
 
