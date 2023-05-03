7a5a9c2bb9cc8024753c2265a7a1ed8e0993a16f
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-03 17:13:30 +0600

    adding recipes json and make in online

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const chefs = require('./chefs.json');
+const recipes = require('./recipe.json');
 
 app.use(cors());
 
@@ -23,6 +24,15 @@ console.log(id);
     res.send(newData);
 })
 
+app.get("/recipes",(req,res) => {
+    res.send(recipes);
+})
+app.get("/recipes/:id",(req,res) => {
+    const id = Number(req.params.id);
+    console.log(id);
+    const newData = recipes.filter(r => r.id === id);
+    res.send(newData)
+})
 
 app.listen(port, () => {
     console.log(`example app listening port ${port}`);
